abe9c383f2770967cfe621629506b4f983c06aa5
thwnd <geozamm1123@gmail.com>
2024-10-28 19:14:20 +0900

    same

--- js/script.js
+++ js/script.js
@@ -61,9 +61,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
         try {
             const formData = new FormData(form);
-
+//https://hknu-ss-awards.netlify.app/
             // Netlify Functions를 호출하도록 URL 설정
-            const response = await fetch('/.netlify/functions/submit-form', {
+            const response = await fetch('../netlify/functions/submit-form', {
                 method: 'POST',
                 body: formData
             });
